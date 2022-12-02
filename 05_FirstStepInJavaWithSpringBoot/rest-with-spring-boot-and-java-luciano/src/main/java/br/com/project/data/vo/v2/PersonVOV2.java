fbcaa942a5723a6418b3ee742987f0cccdb439e0
luciano-mota <amoosenhorjesus3@gmail.com>
2022-12-02 10:32:02 -0300

    Estrutura do Padrão de Projeto Value Object

---
+++ 05_FirstStepInJavaWithSpringBoot/rest-with-spring-boot-and-java-luciano/src/main/java/br/com/project/data/vo/v2/PersonVOV2.java
@@ -0,0 +1,88 @@
+package br.com.project.data.vo.v2;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class PersonVOV2 implements Serializable {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String gender;
+    private Date birthDate;
+
+    public PersonVOV2() {
+
+    }
+
+    public PersonVOV2(Long id, String firstName, String lastName, String adress, String gender) {
+        super();
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = adress;
+        this.gender = gender;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String adress) {
+        this.address = adress;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PersonVOV2 that = (PersonVOV2) o;
+        return Objects.equals(id, that.id) && Objects.equals(firstName, that.firstName) && Objects.equals(lastName, that.lastName) && Objects.equals(address, that.address) && Objects.equals(gender, that.gender) && Objects.equals(birthDate, that.birthDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, address, gender, birthDate);
+    }
+}
